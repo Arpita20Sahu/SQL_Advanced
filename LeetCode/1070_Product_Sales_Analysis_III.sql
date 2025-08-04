7e37c46dc321e6c17aa34ad1377b30a598ebdc92
Arpita Priyadarshini Sahu <83609046+Arpita20Sahu@users.noreply.github.com>
2025-08-04 08:12:03 +0530

    Create 1070_Product_Sales_Analysis_III.sql

---
+++ LeetCode/1070_Product_Sales_Analysis_III.sql
@@ -0,0 +1,68 @@
+/*
+Table: Sales
+
++-------------+-------+
+| Column Name | Type  |
++-------------+-------+
+| sale_id     | int   |
+| product_id  | int   |
+| year        | int   |
+| quantity    | int   |
+| price       | int   |
++-------------+-------+
+(sale_id, year) is the primary key (combination of columns with unique values) of this table.
+product_id is a foreign key (reference column) to Product table.
+Each row records a sale of a product in a given year.
+A product may have multiple sales entries in the same year.
+Note that the per-unit price.
+
+Write a solution to find all sales that occurred in the first year each product was sold.
+
+For each product_id, identify the earliest year it appears in the Sales table.
+
+Return all sales entries for that product in that year.
+
+Return a table with the following columns: product_id, first_year, quantity, and price.
+Return the result in any order.
+
+ 
+
+Example 1:
+
+Input: 
+Sales table:
++---------+------------+------+----------+-------+
+| sale_id | product_id | year | quantity | price |
++---------+------------+------+----------+-------+ 
+| 1       | 100        | 2008 | 10       | 5000  |
+| 2       | 100        | 2009 | 12       | 5000  |
+| 7       | 200        | 2011 | 15       | 9000  |
++---------+------------+------+----------+-------+
+
+Output: 
++------------+------------+----------+-------+
+| product_id | first_year | quantity | price |
++------------+------------+----------+-------+ 
+| 100        | 2008       | 10       | 5000  |
+| 200        | 2011       | 15       | 9000  |
++------------+------------+----------+-------+
+*/
+
+-- Write your MySQL query statement below
+with cte as
+(select product_id, MIN(year) AS first_year 
+from Sales
+group by product_id)
+,
+
+cte2 as
+(select s2.product_id, s2.quantity, s2.price
+from Sales s2
+join cte s1
+on s2.product_id = s1.product_id
+AND s2.year = s1.first_year)
+
+select c.product_id, c.first_year, c2.quantity, c2.price
+from cte c 
+join cte2 c2
+on c.product_id = c2.product_id;
